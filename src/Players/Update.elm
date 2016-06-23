251cdc741fba2875d9fc433d2428a06856dfc0a0
Lesuk <lesuk93@yahoo.com>
2016-06-23 21:03:48 +0300

    Add functions for updating Player level

--- src/Players/Update.elm
+++ src/Players/Update.elm
@@ -3,7 +3,8 @@ module Players.Update exposing (..)
 
 import Navigation
 import Players.Messages exposing (Msg(..))
-import Players.Models exposing (Player)
+import Players.Models exposing (Player, PlayerId)
+import Players.Commands exposing (save)
 
 
 
@@ -21,3 +22,37 @@ update msg players =
 
     ShowPlayer id ->
       ( players, Navigation.modifyUrl ("#players/" ++ (toString id)) )
+
+    ChangeLevel id howMuch ->
+      -- function return a list of commands to run, so we then batch them into one command using Cmd.batch
+      ( players, changeLevelCommands id howMuch players |> Cmd.batch )
+
+    SaveSuccess updatedPlayer ->
+      ( updatePlayer updatedPlayer players, Cmd.none )
+
+    SaveFail error ->
+      ( players, Cmd.none )
+
+
+changeLevelCommands : PlayerId -> Int -> List Player -> List (Cmd Msg)
+changeLevelCommands playerId howMuch =
+  let
+    cmdForPlayer existingPlayer =
+      if existingPlayer.id == playerId then
+        save { existingPlayer | level = existingPlayer.level + howMuch }
+      else
+        Cmd.none
+  in
+    List.map cmdForPlayer
+
+
+updatePlayer : Player -> List Player -> List Player
+updatePlayer updatedPlayer =
+  let
+    select existingPlayer =
+      if existingPlayer.id == updatedPlayer.id then
+        updatedPlayer
+      else
+        existingPlayer
+  in
+    List.map select
