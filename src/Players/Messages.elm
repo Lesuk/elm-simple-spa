11beba847c6873a3b22d12dab09a594a836a491d
Lesuk <lesuk93@yahoo.com>
2016-06-23 18:00:02 +0300

    Update Players Msg

--- src/Players/Messages.elm
+++ src/Players/Messages.elm
@@ -1,6 +1,12 @@
 module Players.Messages exposing (..)
 
+import Http
+import Players.Models exposing (PlayerId, Player)
 
 
+
+-- FetchAllDone will be called when we get the response from the server
+-- FetchAllFail will be called if there is a problem fetching the data.
 type Msg
-  = NoOp
+  = FetchAllDone (List Player)
+  | FetchAllFail Http.Error
